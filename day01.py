30d92a28702cacc7d5b48de12325039058ead7a8
bhargavi Polisetty <sowjanyabhargavi5@gmail.com>
2026-06-12 14:27:02 +0530

    Update day01.py

--- day01.py
+++ day01.py
@@ -1,1 +1,21 @@
-print("practice of day one")
+mark down #python learning journey
+
+DAY:01 OF LEARNING 
+-block diagram of computer
+-memory -memory hierarchy 
+-principles of abstraction
+-language hierarchy 
+
+
+->
+-now python 
+-1.keywords 
+-2.identifires
+-3.variables
+-4.constants 
+-5.literals 
+-6.types of literals 
+-7.datatypes 
+-8.type conversions
+
+thankyouu
